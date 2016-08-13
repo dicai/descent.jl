82d7b9b769af32e2d9e2cc0a20992afc21cc6ad8
Diana Cai <caidcai@gmail.com>
2016-08-13 16:15:52 -0500

    Added trust-region Cauchy point algorithm.

---
+++ optimizers/trustregion.jl
@@ -0,0 +1,115 @@
+function trust_region(x0, deltahat, delta0, eta, f, g, h, max_iter=100, tol=10e-7)
+
+    """
+    x0: vector of the initial starting point of the algorithm
+    deltahat: bound on the step length
+    delta0: starting trust region radius
+    eta: acceptance threshold
+    f: objective function
+    grad: gradient function
+    hess: Hessian function
+    max_iter: maximum number of iterations
+    tol: when to break out
+    """
+
+    @assert deltahat > 0
+    @assert eta >= 0 && eta < 1/4
+    @assert delta0 > 0 && delta0 < deltahat
+
+    delta = delta0
+    n = length(x0)
+    xvals = reshape(x0, (1,n))
+
+
+    for i in 1:max_iter
+        println(i)
+
+        xcurr = xvals[i,:]'[:,1]
+
+        fk = f(xcurr)
+        gk = g(xcurr)
+        Bk = h(xcurr)
+
+        p = solve_subproblem(delta, gk, Bk, "cauchy")
+        rho = compute_rho(xcurr, f, gk, Bk, p)
+
+        # decrease trust region radius
+        if rho < 1/4
+            delta *= 1/4
+
+        else
+            # increase trust region radius
+            if rho > 3/4 && norm(p,2) == delta
+                delta = min(2*delta, deltahat)
+            end
+        end
+
+        # accept the step
+        if rho > eta
+            xnew = xcurr + p
+        # reject the step
+        else
+            xnew = xcurr
+        end
+
+        xvals = vcat(xvals, xnew')
+
+        if abs(mean(xnew - xcurr)) <= tol
+            return xvals
+        end
+
+    end
+    println("Finished algorithm without converging.")
+    return xvals
+
+end
+
+function model(p, xcurr, fk, gk, Bk)
+    """
+    p: vector in which to step in
+    xcurr: vector of the current iterate
+    fk: scalar, f(xk)
+    gk: vector, gradient(xk)
+    Bk: symmetric matrix, e.g., Hessian(xk)
+    """
+    return fk + gk' * p + 1/2 * p' * Bk * p
+end
+
+function compute_rho(xcurr, f, gk, Bk, p)
+    """
+    xcurr: vector of the current iterate
+    f: the objective function
+    gk: gradient(xk)
+    Bk: symmetric matrix
+    p: step
+    """
+    fk = f(xcurr)
+    n = length(p)
+    return ((fk - f(xcurr + p)) ./ (model(zeros(n), xcurr, fk, gk, Bk) - model(p, xcurr, fk, gk, Bk)))[1]
+end
+
+
+
+function solve_subproblem(delta, gk, Bk, method="cauchy")
+    if method == "cauchy"
+        return cauchy_point(delta, gk, Bk)
+    end
+end
+
+
+function cauchy_point(delta, gk, Bk)
+    """
+    delta: positive scalar, trust-region radius at current iteration
+    gk: gradient at current iterate
+    Bk: symmetric matrix, e.g., Hessian
+
+    Returns the cauchy point, a vector.
+    """
+    tau = 1
+    if (gk' * Bk * gk)[1] > 0
+        tau = min(norm(gk,2)^3 ./ (delta * gk'*Bk*gk), 1)[1]
+    end
+
+    # Cauchy point equation
+    return -tau * delta / norm(gk, 2) * gk
+end
